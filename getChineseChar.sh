dff0c55720b28542b6136d706afbc47dbb9ad3e5
Rahul G. Krishnan <rahulk90@gmail.com>
2015-11-03 17:24:22 -0500

    Added files to download and install synthetic and chinese char

---
+++ getChineseChar.sh
@@ -0,0 +1,3 @@
+#Download and unzip files for ChineseCharacters. Follow instructions in README after that
+wget  https://www.dropbox.com/s/9oxtoxfm2y2298s/BarrierFW_NIPS15_ChineseChar.tar.bz2
+tar -xvjf BarrierFW_NIPS15_ChineseChar.tar.bz2
---
+++ getSyntheticPreRun.sh
@@ -0,0 +1,3 @@
+#Download and unzip pre-run synthetic test cases
+wget  https://www.dropbox.com/s/8z5axf978hsczmx/BarrierFW_NIPS15_TestCases.tar.bz2
+tar -xvjf BarrierFW_NIPS15_TestCases.tar.bz2
